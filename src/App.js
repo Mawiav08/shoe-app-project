68a931028b3b0bb425ebf98f816faa6448bf319a
mawiav08 <mawiav008@gmail.com>
2024-07-15 01:20:47 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import HomePage from './components/HomePage';
+import WomensShoes from './components/WomensShoes';
+import MensShoes from './components/MensShoes';
+import KidsShoes from './components/KidsShoes';
+import Cart from './components/Cart';
+import Checkout from './components/Checkout';
+import { CartProvider } from './components/CartContext'; // Correct the path as per your project structure
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <CartProvider>
+        <div className="App">
+          <Navbar />
+          <Routes>  
+            <Route path="/" element={<HomePage />} />
+            <Route path="/womens" element={<WomensShoes />} />
+            <Route path="/mens" element={<MensShoes />} />
+            <Route path="/kids" element={<KidsShoes />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<Checkout />} />
+          </Routes>
+          <Footer />
+        </div>
+      </CartProvider>
+    </Router>
   );
 }
 
 export default App;
+
